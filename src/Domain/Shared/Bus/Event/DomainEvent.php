ba269dfb85ceef748a9c93a67a3ff9c6e055ed2d
Marc Guinea <develop.marcguinea@gmail.com>
2021-09-04 22:58:27 +0200

    fix: namespace uuid vo

--- src/Domain/Shared/Bus/Event/DomainEvent.php
+++ src/Domain/Shared/Bus/Event/DomainEvent.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Domain\Shared\Bus\Event;
 
-use App\Shared\Domain\ValueObject\UuidValueObject;
+use App\Domain\Shared\UuidValueObject;
 use DateTimeImmutable;
 
 abstract class DomainEvent
